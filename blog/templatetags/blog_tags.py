1f61ec9061e86c5b4e3a81ac748d92ea2ab03201
XiongNeng <yidao620@gmail.com>
2014-11-21 18:04:12 +0800

    过滤器和标签，全文显示。

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+Topic: 过滤器filter和标签tag
+Desc : 
+"""
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def more(value, post_id):
+    """通过<!--more-->标签实现文章截取和增加查看全文链接"""
+    more_str = '--more--'
+    if more_str in value:
+        return value.split(more_str)[0] + '<a href="/post/{0}">查看全文</a>'.format(post_id)
+    return value
+
+
+@register.filter
+def lower(value):
+    return value.lower()
+
+
+@register.tag(name='more')
+def do_more(parser, token):
+    """
+    {% more %}
+        This will appear in uppercase, {{ user_name }}.
+    {% endmore %}
+    """
+    nodelist = parser.parse(('endmore',))
+    parser.delete_first_token()
+    return MoreNode(nodelist)
+
+
+class MoreNode(template.Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        output = self.nodelist.render(context)
+        return output.upper()
