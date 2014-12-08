82db0bf913be6a8f4d0f0cb3c87efa7185ba09fe
XiongNeng <yidao620@gmail.com>
2014-12-08 12:03:39 +0800

    定制xadmin后台已经登录页面

--- blog/adminx.py
+++ blog/adminx.py
@@ -5,11 +5,38 @@ Topic: xadmin
 Desc : 
 """
 import xadmin
-from .models import Tag, Category, Post, Comment, Evaluate
+from xadmin import views
+from .models import Tag, Category, Post, Comment, Evaluate, Page
 
+
+class AdminSettings(object):
+    # 设置base_site.html的Title
+    site_title = '博客管理后台'
+    # 设置base_site.html的Footer
+    site_footer = 'Winhong Inc.'
+    menu_style = 'default'
+
+    # 菜单设置
+    def get_site_menu(self):
+        return (
+            {'title': '页面管理', 'perm': self.get_model_perm(Page, 'change'), 'menus': (
+                {'title': '投票', 'url': self.get_model_url(Page, 'changelist')},
+                {'title': '选票', 'url': self.get_model_url(Page, 'changelist')}
+            )},
+        )
+
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, AdminSettings)
 xadmin.site.register(Tag)
 xadmin.site.register(Category)
 xadmin.site.register(Post)
 xadmin.site.register(Comment)
 xadmin.site.register(Evaluate)
+xadmin.site.register(Page)
 
--- blog/models.py
+++ blog/models.py
@@ -72,3 +72,25 @@ class Evaluate(models.Model):
 
     def __str__(self):
         return '{0}: {1}'.format(self.ip, self.evaluate)
+
+
+@python_2_unicode_compatible
+class Page(models.Model):
+    """页面"""
+    # 作者
+    author = models.ForeignKey(User)
+    # 标题
+    title = models.CharField(max_length=200)
+    # 正文
+    text = models.TextField()
+    # 创建时间
+    created_date = models.DateTimeField(default=timezone.now)
+    # 发布时间
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
